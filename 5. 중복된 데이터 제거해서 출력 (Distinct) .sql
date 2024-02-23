c10306f4022b4c4faf9a4eec3cc80d2dc58157af
Harry Park <harrypark960925@gmail.com>
2024-02-23 11:13:25 +0900

    update: 중복된 데이터 제거해서 출력 (distinct) (ORACLE)

--- 5. 중복된 데이터 제거해서 출력 (Distinct) .sql
+++ 5. 중복된 데이터 제거해서 출력 (Distinct) .sql
@@ -10,7 +10,7 @@ from emp;
 
 -- 우리반 테이블 (emp18)에서 통신사를 출력하는데 중복을 제거해서 출력하시오. 
 select distinct telecom
-from emp18;
+from emp18; 
 
 -- 사원 테이블에서 중복이 제거된 부서번호와 직업을 출력하시오. 
 select distinct deptno, job 
