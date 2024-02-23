0bd81dc42ece9363218074367a361a141fb3ce68
Harry Park <harrypark960925@gmail.com>
2024-02-23 11:17:53 +0900

    update: 데이터 정령 (order by) (ORACLE)

--- 6. 데이터 정렬 (order by) .sql
+++ 6. 데이터 정렬 (order by) .sql
@@ -23,12 +23,14 @@ select ename, birth
 from emp18
 order by birth asc;
 
--- 사원 테이블에서 이름과 부서번호와 월급을 출력하는데 부서번호를 ascending 하게 출력된 것을 기준으로 월급을 descending 하게 출력하시오. 
+-- 사원 테이블에서 이름과 부서번호와 월급을 출력하는데 부서번호를 ascending 하게 출력된 것을
+-- 기준으로 월급을 descending 하게 출력하시오. 
 select ename, deptno, sal 
 from emp 
 order by deptno asc, sal desc;
 
--- 이름, 직업, 입사일을 출력하는데 직업을 ABCD 순서대로 정렬해서 출력하고 직업을 ABCD 순서대로 정렬해서 출력한것을 기준으로 두고 입사일을 먼저 입사한 사원부터 출력되게 하시오. 
+-- 이름, 직업, 입사일을 출력하는데 직업을 ABCD 순서대로 정렬해서 출력하고 직업을 ABCD 순서대로 
+-- 정렬해서 출력한것을 기준으로 두고 입사일을 먼저 입사한 사원부터 출력되게 하시오. 
 select ename, job, hiredate 
 from emp 
 order by job, hiredate asc;
@@ -38,9 +40,10 @@ select ename, job, sal +300*2 bonus
 from emp 
 order by bonus desc;
 
--- 우리반 테이블에서 이름과 주소를 출력하는데 주소를 ascending 하게 출력하고, 컬럼명을 한글로 이름, 주소로 출력되게 하시오. 
-select ename 이름, address 주소  
-from emp18 
+-- 우리반 테이블에서 이름과 주소를 출력하는데 주소를 ascending 하게 출력하고,
+-- 컬럼명을 한글로 이름, 주소로 출력되게 하시오. 
+select ename as 이름 , address as 주소 
+from emp18
 order by address asc;
 
 
