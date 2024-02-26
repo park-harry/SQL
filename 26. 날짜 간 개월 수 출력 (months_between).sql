3ba638f5a3d610164469c38a6ad324ab7417441b
Harry Park <harrypark960925@gmail.com>
2024-02-26 23:25:18 +0900

    Update 26. 날짜 간 개월 수 출력 (months_between).sql

--- 26. 날짜 간 개월 수 출력 (months_between).sql
+++ 26. 날짜 간 개월 수 출력 (months_between).sql
@@ -21,3 +21,11 @@ from emp18;
 
 -- 데이터 상으로는 둘의 차이가 없지만 윤년이 적용될 수도 있기 때문에 
 -- months_between이 더 정확한 표현이다. 
+
+-- 우리 반 테이블에서 1993년과 1998년에 태어난 학생들의 이름과 생일과 나이를 출력하는데 
+-- 나이가 높은 학생부터 출력하고 column 명을 한글로 이름, 생일, 나이로 출력하시오.
+-- 어느 나라에 가서도 조회될 수 있도록 SQL을 작성하시오. 
+select ename as 이름, birth as 생일, age as 나이 
+from emp18
+where birth between to_date('1993/1/1','rr/mm/dd') and to_date('1998/12/31','rr/mm/dd')+1
+order by age desc;
